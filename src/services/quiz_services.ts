a846649fdd637e3a707dca4eb058d4fa53da2279
muddassirk <mkkhanu72@gmail.com>
2021-05-25 14:24:56 +0500

    create a shuffleArray and add types

--- src/services/quiz_services.ts
+++ src/services/quiz_services.ts
@@ -1,15 +1,19 @@
 import { Quiz } from '../types/quiz_types'
 
+const shuffleArray = (array: any[]) =>
+[...array].sort(()=> Math.random() - 0.5)
+
+
 export const getQuizDetails = async (totalQuestions: number, level: string) => {
     const res = await fetch(`https://opentdb.com/api.php?amount=${totalQuestions}&difficulty=${level}&type=multiple`);
     let { results } = await res.json();
     // console.log(results);
 
-    const quiz = results.map((questionObj: Quiz) => {
+    const quiz:Quiz[] = results.map((questionObj: Quiz) => {
         return {
             question: questionObj.question,
             answer: questionObj.correct_answer,
-            option: questionObj.incorrect_answers.concat(questionObj.correct_answer)
+            option: shuffleArray(questionObj.incorrect_answers.concat(questionObj.correct_answer))
         }
     })
 
