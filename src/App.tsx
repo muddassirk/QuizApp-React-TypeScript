02641498451edd23c013dcd1aaea296a4d02242b
muddassirk <mkkhanu72@gmail.com>
2021-05-25 14:09:24 +0500

    in useEffect call function getQuizDetails and create new func fetchData

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
+import { getQuizDetails } from './services/quiz_services'
 
 function App() {
+
+  useEffect(() => {
+    getQuizDetails(5, 'easy')
+
+    async function fetchData() {
+      const questions = await getQuizDetails(5, 'easy');
+      console.log(questions); 
+    }
+    fetchData()
+  }, [])
+
+
   return (
     <div className="App">
-     
+      <h1>Hello world</h1>
     </div>
   );
 }
