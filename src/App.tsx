cf246ae30929acb997630542a9c6eab594adda85
muddassirk <mkkhanu72@gmail.com>
2021-05-25 17:24:25 +0500

    alomost completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,45 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import QuestionCard from './Components/QuestionCard';
 import { getQuizDetails } from './services/quiz_services'
+import {Quiz, QuestionType} from './types/quiz_types'
 
 function App() {
+
+  let [quiz, setQuiz] = useState<QuestionType[]>([])
+  let [currentStep, setCurrentStep] = useState(0)
 
   useEffect(() => {
     getQuizDetails(5, 'easy')
 
     async function fetchData() {
-      const questions = await getQuizDetails(5, 'easy');
+      const questions:QuestionType[] = await getQuizDetails(5, 'easy');
       console.log(questions); 
+      setQuiz(questions)
     }
     fetchData()
   }, [])
 
 
+  const handleSubmit = (e:React.FormEvent<EventTarget>)=>{
+    e.preventDefault();
+    if(currentStep !== quiz.length-1)
+    setCurrentStep(++currentStep)
+    else{
+      alert("Quiz completed");
+      setCurrentStep(0)
+    }
+  }
+
+  if(!quiz.length) return <h4>Loading...</h4>
+
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <QuestionCard
+        options={quiz[currentStep].option}
+        question={quiz[currentStep].question}
+        callback={handleSubmit}
+      />
     </div>
   );
 }
