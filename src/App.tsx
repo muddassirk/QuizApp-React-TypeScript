ad5e263aa7b44ec058e820c69d67754da1ed7d65
muddassirk <mkkhanu72@gmail.com>
2021-05-27 23:35:16 +0500

    user Score/Result

--- src/App.tsx
+++ src/App.tsx
@@ -2,32 +2,40 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 import QuestionCard from './Components/QuestionCard';
 import { getQuizDetails } from './services/quiz_services'
-import {Quiz, QuestionType} from './types/quiz_types'
+import { QuestionType} from './types/quiz_types'
 
 function App() {
 
   let [quiz, setQuiz] = useState<QuestionType[]>([])
   let [currentStep, setCurrentStep] = useState(0)
+  let [score, setScore] = useState(0)
 
   useEffect(() => {
     getQuizDetails(5, 'easy')
 
     async function fetchData() {
       const questions:QuestionType[] = await getQuizDetails(5, 'easy');
-      console.log(questions); 
       setQuiz(questions)
     }
     fetchData()
   }, [])
 
 
-  const handleSubmit = (e:React.FormEvent<EventTarget>)=>{
+  const handleSubmit = (e:React.FormEvent<EventTarget>, userAns:string)=>{
+    const currentQuestion: QuestionType = quiz[currentStep]
+    console.log('correct answ: '+ currentQuestion.answer + " -user Select Ans: " + userAns);
+
+    if(userAns === currentQuestion.answer ){
+      setScore(++score)
+    }
+    
     e.preventDefault();
     if(currentStep !== quiz.length-1)
     setCurrentStep(++currentStep)
     else{
-      alert("Quiz completed");
+      alert("Your Score is "+ score + " out of: " + quiz.length);
       setCurrentStep(0)
+      setScore(0)
     }
   }
 
