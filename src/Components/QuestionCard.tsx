e8ecc10973a1e5fe0b2bbe13e7a022a2c27cc4c2
muddassirk <mkkhanu72@gmail.com>
2021-05-27 23:34:36 +0500

    useState, handleSelectionAns, and add one props in onSubmit

--- src/Components/QuestionCard.tsx
+++ src/Components/QuestionCard.tsx
@@ -1,18 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { questionPropsType } from '../types/quiz_types'
 import './QuestionCard.css'
 
 const QuestionCard: React.FC<questionPropsType> = ({ question, options, callback }) => {
+
+    const [ selectedAns, setSelectedAnd] = useState('')
+
+    const handleSelectedAns = (ev: any) =>{
+        // console.log(ev.target.value);
+        setSelectedAnd(ev.target.value)
+        
+    }
+
+
     return (
         <div className='container'>
             <div className='ques-container'>
                 <div className='head-container'>
                     <h1>Quiz app</h1>
                 </div>
-                <div>
+                <h3>
                     {question}
-                </div>
-                <form onSubmit={callback}>
+                </h3>
+                <form onSubmit={(e:React.FormEvent<EventTarget>)=>callback(e, selectedAns)}>
                     {
                         options.map((opt: string, ind: number) => {
                             return (
@@ -22,6 +32,9 @@ const QuestionCard: React.FC<questionPropsType> = ({ question, options, callback
                                             type="radio"
                                             name="option"
                                             value={opt}
+                                            required
+                                            checked={selectedAns === opt}
+                                            onChange={handleSelectedAns}
                                         />
                                         {opt}
                                     </label>
